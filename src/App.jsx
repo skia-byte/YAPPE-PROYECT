66e2a35c7b1782ce4cd83f7ba7a41749296df014
Bianca <biaberechemeza@crackthecode.la>
2026-04-18 16:37:27 -0500

    ultimos detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,36 @@
+
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
-// 1. IMPORTACIONES DE CONTEXTO
 import { MascotProvider } from "./context/MascotContext";
-import { useAuth } from "./context/authContext";
-
-// 2. IMPORTACIÓN DE COMPONENTES DE DISEÑO
-import YapeMascot from "./componentes/YapeMascot/YapeMascot";
 import MainLayout from "./layouts/MainLayout";
+import YapeMascot from "./componentes/YapeMascot/YapeMascot";
 import "./App.css";
-import Login from './componentes/Login';
 
-// PÁGINAS CON LAZY LOADING
+// Lazy Loading
 const Inicio = lazy(() => import("./paginas/Index"));
 const Nosotros = lazy(() => import("./paginas/Nosotros"));
 const CentrodeAyuda = lazy(() => import("./paginas/CentrodeAyuda"));
-const Unete = lazy(() => import("./paginas/Unete")); // Cambiado de Productos a Unete
+const Unete = lazy(() => import("./paginas/Unete"));
 const Perfil = lazy(() => import("./paginas/Perfil"));
 const LibroDeReclamaciones = lazy(() => import("./paginas/LibroDeReclamaciones"));
-
-// 3. IMPORTACIÓN DE VER DETALLES (Ruta ajustada a componentes)
 const VerDetalles = lazy(() => import("./componentes/VerDetalles"));
-const FormularioDePostulacion = lazy(() => import("./paginas/FormularioDePostulacion"));
+const FormularioDePostulacion = lazy(() =>
+  import("./paginas/FormularioDePostulacion")
+);
 
 function App() {
   return (
     <MascotProvider>
-      <Suspense
-        fallback={
-          <div style={{ 
-            textAlign: "center", 
-            marginTop: "10rem", 
-            fontFamily: "sans-serif", 
-            fontWeight: "900", 
-            color: "#7e1d91",
-            fontStyle: "italic" 
-          }}>
-            Yo yapeo, tú yapeas, todos yapeamos ...
-          </div>
-        }
-      >
+      <Suspense fallback={<h1>Cargando...</h1>}>
         <Routes>
           <Route element={<MainLayout />}>
             <Route path="/" element={<Inicio />} />
             <Route path="/nosotros" element={<Nosotros />} />
             <Route path="/centrodeayuda" element={<CentrodeAyuda />} />
-            <Route path="/unete" element={<Unete />} /> {/* Ahora llama al componente Unete */}
+            <Route path="/unete" element={<Unete />} />
             <Route path="/perfil/:username" element={<Perfil />} />
             <Route path="/libro-de-reclamaciones" element={<LibroDeReclamaciones />} />
-            
-
-            {/* RUTA VINCULADA */}
             <Route path="/detalles-empleo/:id" element={<VerDetalles />} />
             <Route path="/postular/:id" element={<FormularioDePostulacion />} />
           </Route>
